d9aa8dbb2537f1b6d04ee7d097ce5d33fe827d84
Keshav <justparihar04@gmail.com>
2023-01-12 17:28:53 +0530

    Ch2_complted

--- Project_1/test_app/lib/main.dart
+++ Project_1/test_app/lib/main.dart
@@ -11,12 +11,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Container(
-        child: Center(child: Text("Hello Keshav")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Container(
+          color: Color.fromARGB(210, 5, 0, 0),
+          width: 200,
+          height: 310,
+          child :Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.orange,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.white,
+                width: 100,
+                height: 100,
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.green,
+                width: 100,
+                height: 100,
+              ),
+            ],
+          ),
+        )
+        );
   }
 }
